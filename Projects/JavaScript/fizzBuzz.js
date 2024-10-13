114d7cfc6baaa1738162a3725c0301c66405c558
Valdirlei Jose <valdirleijose@gmail.com>
2024-10-13 20:54:44 +0100

    Update fizzBuzz.js

--- Projects/JavaScript/fizzBuzz.js
+++ Projects/JavaScript/fizzBuzz.js
@@ -1,19 +1,29 @@
 
 var numbers = [];
 var count = 0;
-
+var f = "Fizz";
+var b = "Buzz";
+var fb = "FizzBuzz";
 function fizzBuzz() {
 
     for (let index = 1; index < 101; index++) {
 
-        if (index%3===0) || (index%5===0) {
-            count = "fizz";
-        }
-        if (index%5===0) {
-            count = "buzz";
+        if (index%3==0 || index%5==0 ) {
+            if (index%3 == 0) {
+                
+                numbers.push(f+" "+index)
+            } 
+            if (index%5 == 0) {
+                numbers.push(b+" "+index)
+                
+                
+            }if(index%3 == 0 && index%5 == 0) {
+                numbers.push(fb+" "+index)
+                 
+            }
         }
         else{
-            count = index;
+        count = index;
         numbers.push(count);
         }
         
@@ -26,3 +36,5 @@ function fizzBuzz() {
 }
 
 fizzBuzz();
+
+
