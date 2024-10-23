0eb9e51c56a5f04f31c458e841a2e32a6c9e3ff7
Valdirlei Jose <valdirleijose@gmail.com>
2024-10-23 15:30:13 +0100

    updated index

--- Projects/DrumApp/Drum Kit Starting Files/index.js
+++ Projects/DrumApp/Drum Kit Starting Files/index.js
@@ -1,5 +1,11 @@
-
-
-function handleClick(params) {
-    alert("I got clicked!")
+// document.querySelector("button").addEventListener("click", function handleClick() {
+//     alert("I got clicked!")
+// });
+for (let index = 0; index < document.querySelectorAll(".drum").length; index++) {
+    document.querySelectorAll("button")[index].addEventListener("click", function handleClick() {
+        alert("I got clicked!")});
+    
 }
+
+
+
