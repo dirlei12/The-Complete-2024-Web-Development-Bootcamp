aff332079cb51a5761c469be0d2e87b0aefef48c
Valdirlei Jose <valdirleijose@gmail.com>
2024-10-29 19:50:01 +0000

    s

--- Projects/DrumApp/Drum Kit Starting Files/index.js
+++ Projects/DrumApp/Drum Kit Starting Files/index.js
@@ -1,11 +1,16 @@
 // document.querySelector("button").addEventListener("click", function handleClick() {
 //     alert("I got clicked!")
 // });
-for (let index = 0; index < document.querySelectorAll(".drum").length; index++) {
-    document.querySelectorAll("button")[index].addEventListener("click", function handleClick() {
-        alert("I got clicked!")});
-    
+for (
+  let index = 0;
+  index < document.querySelectorAll(".drum").length;
+  index++
+) {
+  document
+    .querySelectorAll(".drum")
+    [index].addEventListener("click", function () {
+      
+        var audio = new Audio('tom-1.mp3');
+        audio.play();
+    });
 }
-
-
-
