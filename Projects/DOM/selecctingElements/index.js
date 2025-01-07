f87e2d7d753717bf2a427d59e8daa71c4aa4ecbc
Valdirlei Jose <valdirleijose@gmail.com>
2025-01-07 17:00:10 +0000

    bdvx

---
+++ Projects/DOM/selecctingElements/index.js
@@ -0,0 +1,27 @@
+// // Seleciona os elementos necessários
+// lista = document.querySelector("#list").lastElementChild;
+// checkbox = document.querySelector("input");
+
+// // Adiciona o evento para monitorar mudanças no checkbox
+// checkbox.addEventListener("change", () => {
+//   if (checkbox.checked === true) {
+//     lista.innerHTML = "<li>Botao foi clicado</li>";
+//   } else {
+//     lista.innerHTML = `
+      
+//       <li class="list">Third</li>
+//     `;
+//   }
+// });
+
+// listas = document.getElementsByTagName("li");
+
+// alert(document.getElementsByTagName("li").length);
+
+
+document.querySelector(".list a").style.color = "red";
+
+document.querySelectorAll("#list .list")[1].style.color = "purple";
+
+document.querySelector("#list").lastElementChild.style.color = "blue";
+
