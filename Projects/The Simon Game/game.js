3a4d4af8108adefed9170b02a3f7feae9073702c
Valdirlei Jose <valdirleijose@gmail.com>
2025-01-16 06:54:57 +0000

    16/01/2025 7:00am

--- Projects/The Simon Game/game.js
+++ Projects/The Simon Game/game.js
@@ -1,15 +1,42 @@
+//array to save the pattern
 let gamePattern = [];
 
+//array where the colour buttons are
 let buttonCoulors = ["red", "blue", "green", "yellow"];
 
-var randomChosenColour = buttonCoulors[nextSequence()];
 
+
+//function to get a randon number from 0 to 3
 function nextSequence() {
     var randonNumber = Math.floor(Math.random() * 4);
 
     return randonNumber;
 }
 
-gamePattern.push(randomChosenColour);
 
-console.log(nextSequence());
+
+
+
+
+$(".play").on("click", function(){
+
+    nextSequence();
+    //variable to save the randon chosen colour
+    var randomChosenColour = buttonCoulors[nextSequence()];
+
+   // add the randon chosen colour to the array gamePatern
+    gamePattern.push(randomChosenColour);  
+});
+
+// event to check the game patern
+$(".ma").on("click", function(){
+    
+    for (let index = 0; index < gamePattern.length; index++) {
+        alert(gamePattern)[index];   
+        
+    }
+       
+});
+
+
+
