3206935007f98722a7f4eccaf60ba83a3a897a8b
Valdirlei Jose <valdirleijose@gmail.com>
2025-02-06 17:52:37 +0000

    czc

---
+++ Projects/Node/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,36 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// var name = generateName();
+
+// console.log(name);
+
+import superheroes from 'superheroes';
+import {randomSuperhero} from 'superheroes';
+
+superheroes;
+//=> ['3-D Man', 'A-Bomb', …]
+
+let name = randomSuperhero();
+//=> 'Spider-Ham'
+
+console.log(name);
+
+// import {catNames, randomCatName} from 'cat-names';
+
+// catNames;
+// //=> ['Abby', 'Angel', …]
+
+// randomCatName();
+// //=> 'Max'
+
+// for (let index = 0; index < 5; index++) {
+//     catNames[index] = randomCatName();
+    
+// }
+// for (let index = 0; index < 5; index++) {
+//     console.log(catNames[index]+" ");
+    
+// }
+
+
